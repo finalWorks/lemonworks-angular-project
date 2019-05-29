3fb5189364eede6f00e28ecc1e134057b51f0c87
liangzhongyu <34783362+liangzhongyu@users.noreply.github.com>
2019-05-29 22:14:38 +0800

    HTML属性绑定学习实例

--- src/app/demo/bind-demo/html-attribute-bind/html-attribute-bind.component.ts
+++ src/app/demo/bind-demo/html-attribute-bind/html-attribute-bind.component.ts
@@ -7,9 +7,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HtmlAttributeBindComponent implements OnInit {
 
+  private greeting: String = 'A value';
+  private cssKey: String = '';
+  private isB: Boolean = false;
+  private isC: Boolean = false;
+  private isD: Boolean = false;
+  private isE: Boolean = false;
+
   constructor() { }
 
   ngOnInit() {
+    this.cssKey = 'styleA';
   }
 
+  clickEvent1(event: any) {
+    this.greeting = 'B value';
+  }
+
+  clickEvent2(event: any) {
+    this.isB = !this.isB;
+  }
+
+  clickEvent3(event: any) {
+    this.isE = !this.isE;
+  }
+
+  inputEvent(event: any) {
+    const cssKey: String = event.target.value;
+    if (cssKey === 'C' || cssKey === 'c') {
+      this.isC = true;
+    } else if (cssKey === 'D' || cssKey === 'd') {
+      this.isD = true;
+    } else {
+      this.isC = false;
+      this.isD = false;
+    }
+  }
 }

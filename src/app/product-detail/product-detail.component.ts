fa3817cad4433b65ff4ebf4f31a9575093586abb
liangzhongyu <34783362+liangzhongyu@users.noreply.github.com>
2019-06-08 12:36:41 +0800

    组件间通信学习实例

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -11,12 +11,31 @@ export class ProductDetailComponent implements OnInit {
 
   private product: Product;
   private comments: Comment[];
+
+  newRating: number = 5;
+  newComment: string = '';
+
+  isCommentHidden: Boolean = true;
+
   constructor(private routeInfo: ActivatedRoute, private productService: ProductService) { }
 
   ngOnInit() {
     const productId: number = Number('' + this.routeInfo.snapshot.params['productId']);
     this.product = this.productService.getProduct(productId);
     this.comments = this.productService.getProductComments(productId);
+    let sum = this.comments.reduce((sum, comment) => sum + comment.rating, 0);
+    this.product.rating = sum / this.comments.length;
   }
 
+  addComment() {
+    let comment = new Comment(0, this.product.id, new Date().toISOString(), 'Coco', this.newRating, this.newComment);
+    this.comments.unshift(comment);
+    // reduce是一个数组循环方法，需要两个参数：第一个是回调函数（参数二comment是当前处理的数元素），
+    // 第二个是回调函数参数一sum的初期值。
+    let sum = this.comments.reduce((sum, comment) => sum + comment.rating, 0);
+    this.product.rating = sum / this.comments.length;
+    this.newRating = 5;
+    this.newComment = '';
+    this.isCommentHidden = true;
+  }
 }
--- src/app/stars/stars.component.ts
+++ src/app/stars/stars.component.ts
@@ -1,22 +1,40 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-stars',
   templateUrl: './stars.component.html',
   styleUrls: ['./stars.component.css']
 })
-export class StarsComponent implements OnInit {
+export class StarsComponent implements OnInit, OnChanges {
 
   @Input()
-  private rating: Number = 0;
+  private rating: number = 0;
+  @Input()
+  private readOnly: Boolean = true;
+
+  // 输出属性的名字是输入属性名加Change的时候，才可以通过输入属性名进行双向绑定。
+  @Output()
+  private ratingChange: EventEmitter<number> = new EventEmitter();
+
   private stars: boolean[];
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
     this.stars = [true, true, true, true, true];
     for (let i = 0; i < this.rating; i++) {
       this.stars[i] = false;
     }
   }
 
+  clickStar(index: number) {
+    if (!this.readOnly) {
+      this.rating = index + 1;
+      this.ngOnInit();
+      this.ratingChange.emit(this.rating);
+    }
+  }
 }
